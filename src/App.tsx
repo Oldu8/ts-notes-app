a0c2f446f123f4cf5e368eae3dc7ca0c5e1059b5
Oleh <dudkoleg@gmail.com>
2023-01-03 12:45:25 -0500

    note form page

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NewNote from "./NewNote";
+import useLocalStorage from "./useLocalStorage";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+export type RawNote = {
+  id: string;
+};
+
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id" element={<h1>Hi id</h1>}>
+          <Route index element={<h1>Show id</h1>} />
+          <Route element={<h1>Edit id</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/NewNote.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NoteForm from "./NoteForm";
+
+function NewNote() {
+  return <div className="mb-4">{/* <NoteForm />////> */}</div>;
+}
+
+export default NewNote;
