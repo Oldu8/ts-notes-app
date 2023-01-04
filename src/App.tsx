9c67e2bb3420ca35f1f0daac071bc986ac367644
Oleh <dudkoleg@gmail.com>
2023-01-04 11:55:27 -0500

    Note Forms + NewNote func comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useMemo } from "react";
 import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewNote from "./NewNote";
 import useLocalStorage from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
 
 export type Note = {
   id: string;
@@ -27,17 +29,47 @@ export type Tag = {
 
 export type RawNote = {
   id: string;
-};
+} & RawNoteData;
 
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...notes,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Hi</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
         <Route path="/:id" element={<h1>Hi id</h1>}>
           <Route index element={<h1>Show id</h1>} />
           <Route element={<h1>Edit id</h1>} />
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { NoteData, Tag } from "./App";
 import NoteForm from "./NoteForm";
 
-function NewNote() {
-  return <div className="mb-4">{/* <NoteForm />////> */}</div>;
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
+    <div className="mb-4">
+      {
+        <NoteForm
+          onSubmit={onSubmit}
+          onAddTag={onAddTag}
+          availableTags={availableTags}
+        />
+      }
+    </div>
+  );
 }
 
 export default NewNote;
